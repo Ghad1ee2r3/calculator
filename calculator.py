957e5e195cfbb298079072a27608f6ffd9be0ad2
Ghad1ee2r3 <52378925+Ghad1ee2r3@users.noreply.github.com>
2020-08-17 10:55:20 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,31 @@
 
 def main():
 	#write your code here
-	
+
 	num1= input("Enter a number one:")
 	num2= input("Enter a number two:")
-	int_num1= int(num1)
-	int_num2= int(num2)
+	if num1.isdigit() and num2.isdigit() :
+		int_num1= int(num1)
+		int_num2= int(num2)
+		op= input("Choose the operation (+, -, /, *): ")
+		if op=="+":
+			print(int_num1+int_num2)
+		elif op=="-":
+			print( int_num1-int_num2)
+		elif op=="*":
+			print( int_num1*int_num2)
+		elif op=="/":
+			print( int_num1/int_num2)
+		else:
+			print( "you are enter invalid operation")
 
-	op= input("Choose the operation (+, -, /, *): ")
-	if op=="+":
-		print(int_num1+int_num1)
-	elif op=="-":
-		print( int_num1-int_num1)
-	elif op=="*":
-		print( int_num1*int_num1)
-	elif op=="/":
-		print( int_num1/int_num1)
 	else:
-		print( "you are enter invalid operation")
+		print(" invalid")
+	#int_num1= int(num1)
+	#int_num2= int(num2)
+
+
+
 
 
 	#if op=="+":
