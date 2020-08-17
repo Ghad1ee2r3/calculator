bc1c38b23a742ae2b1f618437a32d5ead439df50
Ghad1ee2r3 <52378925+Ghad1ee2r3@users.noreply.github.com>
2020-08-17 09:46:50 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,28 @@
 def main():
 	#write your code here
 	pass
+
+	#write your code here
+	
+	num1= input("Enter a number one:")
+	num2= input("Enter a number two:")
+	int_num1= int(num1)
+	int_num2= int(num2)
+
+	op= input("Choose the operation (+, -, /, *): ")
+	if op=="+":
+		print(int_num1+int_num1)
+	else:
+		if op=="-":
+			print( int_num1-int_num1)
+		else:
+			if op=="*":
+				 print( int_num1*int_num1)
+			else:
+				if op=="/":
+					print( int_num1/int_num1)
+
+
 	
 
 
